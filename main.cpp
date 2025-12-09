8713ad219e05c34783140782a6b06a2437473fa1
Ultragodop <ultragodop@gmail.com>
2025-12-09 10:56:55 -0300

    little test, and refactor para no arruinar tu code:D

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
+#include <chrono>
 #include <iostream>
 #include <string>
 #include <variant>
 using val = std::variant<int, std::string>;
-
+void tests();
 int main() {
-  auto lang = "C++";
-  for (int i = 1; i <= 5; i++) {
-    std::cout << "i = " << i;
-  }
+
   val v1 = 10;
   val v2 = "C++";
+
+  tests();
+
 
   // value se mueve
   // 
@@ -44,3 +45,34 @@ int main() {
 //instead of using auto types, declare the variables in the types tthat the variable is going to be used fuckin moron
   return 0;
 }
+
+
+
+void tests() {
+   const std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
+  for (int i = 1; i <= 10000; i++) {
+
+    if (i & 1) {
+      printf("%d es puto fracasado y no es par\n", i);
+
+    }
+
+
+
+  }const std::chrono::system_clock::time_point stop = std::chrono::system_clock::now();
+  const long time = std::chrono::duration_cast<std::chrono::seconds>(stop - start).count();
+    std::cout << "El tiempo es: "<< time << " ms"<< '\n';
+  std::cout << "Ahora con el fuckin % " << '\n';
+    const std::chrono::system_clock::time_point start1 = std::chrono::system_clock::now();
+  for (int x = 1; x <= 10000; x++) {
+      if (x%2!=0) {
+        printf("%d es puto fracasado y no es par\n", x);
+      }
+
+
+    }
+const std::chrono::system_clock::time_point stop1 = std::chrono::system_clock::now();
+    const long time1 = std::chrono::duration_cast<std::chrono::seconds>(stop1 - start1).count();
+  std::cout << "El tiempo es: "<< time1 <<" ms"<< '\n';
+}
+
